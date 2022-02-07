21fde904ba06ec038fdadf01ec060ecf5ba11ec0
Nikita Tulenev <n1tulenev@gmail.com>
2022-02-07 22:55:54 +0300

    Add Null key sender logic

--- src/Logic/KafkaSender.cs
+++ src/Logic/KafkaSender.cs
@@ -21,14 +21,27 @@ namespace Logic
 
         public async Task SendAsync<TKey>(Topic topic, Message<TKey> message, CancellationToken ct)
         {
-            using var p = new ProducerBuilder<TKey, string>(_config).Build();
-            var dr = await p.ProduceAsync(topic.Name, new Message<TKey, string>
+            if (message.Key is not null)
             {
-                Key = message.Key,
-                Value = message.Value
+                using var p = new ProducerBuilder<TKey, string>(_config).Build();
+                var dr = await p.ProduceAsync(topic.Name, new Message<TKey, string>
+                {
+                    Key = message.Key,
+                    Value = message.Value
+                }
+                , ct)
+                .ConfigureAwait(false);
             }
-            , ct)
-            .ConfigureAwait(false);
+            else
+            {
+                using var p = new ProducerBuilder<Null, string>(_config).Build();
+                var dr = await p.ProduceAsync(topic.Name, new Message<Null, string>
+                {
+                    Value = message.Value
+                }
+                , ct)
+                .ConfigureAwait(false);
+            }
         }
 
         private readonly ProducerConfig _config;
