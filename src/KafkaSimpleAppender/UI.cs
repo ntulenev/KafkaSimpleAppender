acc7da2fc59d10c0b7fa1d3e03a7165bd2203938
Nikita Tulenev <n1tulenev@gmail.com>
2022-02-07 22:00:31 +0300

    Add UI updates

--- src/KafkaSimpleAppender/UI.cs
+++ src/KafkaSimpleAppender/UI.cs
@@ -14,19 +14,47 @@ namespace KafkaSimpleAppender
 
         private readonly IKafkaSender _sender;
 
+#pragma warning disable IDE1006 // Naming Styles
         private async void bSend_Click(object sender, EventArgs e)
+#pragma warning restore IDE1006 // Naming Styles
         {
             try
             {
+                DisableUI();
+
                 var topic = new Topic(tbTopic.Text);
                 var message = new Models.Message(tbKey.Text, tbMessage.Text);
 
                 await _sender.SendAsync(topic, message, CancellationToken.None);
+
+                Clear();
+
+                MessageBox.Show("Success", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
+            finally
+            {
+                EnableUI();
+            }
+        }
+
+        private void DisableUI()
+        {
+            Enabled = false;
+        }
+
+        private void EnableUI()
+        {
+            Enabled = true;
+        }
+
+        private void Clear()
+        {
+            tbKey.Clear();
+            tbMessage.Clear();
         }
     }
 }
