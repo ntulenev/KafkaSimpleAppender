41789cad4b72f6119995e97b63b9fd2c444c2552
Nikita Tulenev <n1tulenev@gmail.com>
2022-02-07 21:30:38 +0300

    Add BootstrapConfiguration

---
+++ src/Logic/Configuration/BootstrapConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using Confluent.Kafka;
+
+namespace Logic.Configuration
+{
+    /// <summary>
+    /// bootstrap servers config
+    /// </summary>
+    public class BootstrapConfiguration
+    {
+        /// <summary>
+        /// List of bootstrap servers
+        /// </summary>
+        public List<string> BootstrapServers { get; set; } = default!;
+
+        /// <summary>
+        /// Kafka user name, is any.
+        /// </summary>
+        public string? Username { get; set; }
+
+        /// <summary>
+        /// Kafka password name, is any.
+        /// </summary>
+        public string? Password { get; set; }
+
+        /// <summary>
+        /// Kafka security protocol.
+        /// </summary>
+        public SecurityProtocol SecurityProtocol { get; set; } = SecurityProtocol.Plaintext;
+
+        /// <summary>
+        /// Kafka security protocol mechanism.
+        /// </summary>
+        public SaslMechanism? SASLMechanism { get; set; }
+    }
+}
