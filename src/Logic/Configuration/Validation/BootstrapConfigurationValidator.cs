02317631815ce1ae1ca525dcfdceee479e945f8c
Nikita Tulenev <n1tulenev@gmail.com>
2022-02-07 21:30:45 +0300

    Add BootstrapConfigurationValidator

---
+++ src/Logic/Configuration/Validation/BootstrapConfigurationValidator.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics;
+
+using Microsoft.Extensions.Options;
+
+namespace Logic.Configuration.Validation
+{
+    /// <summary>
+    /// Validator for <see cref="BootstrapConfiguration"/>.
+    /// </summary>
+    public class BootstrapConfigurationValidator : IValidateOptions<BootstrapConfiguration>
+    {
+        /// <summary>
+        /// Validates <see cref="KafkaServiceClientConfiguration"/>.
+        /// </summary>
+        public ValidateOptionsResult Validate(string name, BootstrapConfiguration options)
+        {
+            Debug.Assert(name is not null);
+            Debug.Assert(options is not null);
+
+            if (options.BootstrapServers is null)
+            {
+                return ValidateOptionsResult.Fail("BootstrapServers section is not set.");
+            }
+
+            if (!options.BootstrapServers.Any())
+            {
+                return ValidateOptionsResult.Fail("BootstrapServers section is empty.");
+            }
+
+            if (options.BootstrapServers.Any(x => String.IsNullOrEmpty(x)))
+            {
+                return ValidateOptionsResult.Fail("BootstrapServers section contains empty string.");
+            }
+
+            if (options.BootstrapServers.Any(x => String.IsNullOrWhiteSpace(x)))
+            {
+                return ValidateOptionsResult.Fail("BootstrapServers section contains empty string of whitespaces.");
+            }
+
+            return ValidateOptionsResult.Success;
+        }
+    }
+}
