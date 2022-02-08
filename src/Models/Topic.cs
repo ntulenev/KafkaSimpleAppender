6f39761b4ab0442e10eaf6814652ebe1abb75789
Nikita Tulenev <n1tulenev@gmail.com>
2022-02-08 19:06:03 +0300

    Add topic name validation

--- src/Models/Topic.cs
+++ src/Models/Topic.cs
@@ -1,4 +1,48 @@
-﻿namespace Models
+﻿using System.Text.RegularExpressions;
+
+namespace Models
 {
-    public record Topic(string Name);
+    public class Topic
+    {
+        public string Name { get; }
+        public Topic(string name)
+        {
+            ValidateName(name);
+            Name = name;
+        }
+
+        private static void ValidateName(string name)
+        {
+            if (name is null)
+            {
+                throw new ArgumentNullException(nameof(name), "Topic name is not set.");
+            }
+
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentException("The topic name cannot be empty or consist of whitespaces.", nameof(name));
+            }
+
+            if (name.Any(character => char.IsWhiteSpace(character)))
+            {
+                throw new ArgumentException("The topic name cannot contain whitespaces.", nameof(name));
+            }
+
+            if (name.Length > MAX_TOPIC_NAME_LENGTH)
+            {
+                throw new ArgumentException("The name of a topic is too long.", nameof(name));
+            }
+
+            if (!_topicNameCharacters.IsMatch(name))
+            {
+                throw new ArgumentException("Incorrect topic name. The topic name may consist of characters 'a' to 'z', 'A' to 'Z', digits, and minus signs.", nameof(name));
+            }
+        }
+
+        private static readonly Regex _topicNameCharacters = new(
+            "^[a-zA-Z0-9\\-]*$",
+            RegexOptions.Compiled);
+
+        private const int MAX_TOPIC_NAME_LENGTH = 249;
+    }
 }
