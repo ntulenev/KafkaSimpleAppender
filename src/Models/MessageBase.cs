0c592a42c17a9cad96ff4fc0c4ae1e64e708e7a3
Nikita Tulenev <n1tulenev@gmail.com>
2022-02-09 19:00:08 +0300

    Add simple payload validation

--- src/Models/MessageBase.cs
+++ src/Models/MessageBase.cs
@@ -6,6 +6,16 @@
 
         public MessageBase(string payload)
         {
+            if (payload is null)
+            {
+                throw new ArgumentNullException(nameof(payload), "Topic name is not set.");
+            }
+
+            if (string.IsNullOrEmpty(payload))
+            {
+                throw new ArgumentException("The topic name cannot be empty or consist of whitespaces.", nameof(payload));
+            }
+
             Payload = payload;
         }
     }
