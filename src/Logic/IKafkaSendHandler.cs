2916d86c1e3ffef5b0f761eb9af5eed54ec95216
Nikita Tulenev <n1tulenev@gmail.com>
2022-02-09 19:12:00 +0300

    Add SenderHandler

---
+++ src/Logic/IKafkaSendHandler.cs
@@ -0,0 +1,11 @@
+﻿using Models;
+
+namespace Logic
+{
+    public interface IKafkaSendHandler
+    {
+        public Task HandleAsync(string topicName, object keyType, string key, string payload, CancellationToken ct);
+
+        public IEnumerable<KeyType> ValidKeyTypes { get; }
+    }
+}
