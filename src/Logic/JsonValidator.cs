0a91f48fecd257c65d80869958472d43075fd642
Nikita Tulenev <n1tulenev@gmail.com>
2022-02-10 18:32:53 +0300

    Add simple json validator

---
+++ src/Logic/JsonValidator.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json;
+
+namespace Logic
+{
+    public class JsonValidator : IJsonValidator
+    {
+        public bool IsValid(string value)
+        {
+            ArgumentNullException.ThrowIfNull(value);
+
+            try
+            {
+                _ = JsonDocument.Parse(value);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
