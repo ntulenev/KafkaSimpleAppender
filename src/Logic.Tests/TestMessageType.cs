6b78e2340796d96eb5afa6570295fd0c3faad580
Nikita Tulenev <n1tulenev@gmail.com>
2022-02-15 10:27:08 +0300

    Add test message type

---
+++ src/Logic.Tests/TestMessageType.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+using Models;
+
+namespace Logic.Tests
+{
+    /// <summary>
+    /// Kafka message for test
+    /// </summary>
+    public class TestMessageType : MessageBase<object>
+    {
+        /// <summary>
+        /// Creates <see cref="TestMessageType"/>
+        /// </summary>
+        /// <param name="payload">Mesasge text.</param>
+        /// <exception cref="ArgumentNullException">Thows if payload is null.</exception>
+        /// <exception cref="ArgumentException">Throws if payload is empty.</exception>
+        public TestMessageType(string payload) : base(payload)
+        {
+
+        }
+    }
+}
