347a40cb8b1e2b8dc5771c16f53eef49a628085e
Nikita Tulenev <n1tulenev@gmail.com>
2022-02-20 16:15:35 +0300

    Add KafkaSendHandler tests

--- src/Logic.Tests/KafkaSendHandler.Tests.cs
+++ src/Logic.Tests/KafkaSendHandler.Tests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 using FluentAssertions;
 
@@ -81,6 +83,99 @@ namespace Logic.Tests
             keyTypes.Should().Contain(Models.KeyType.NotSet);
             keyTypes.Should().Contain(Models.KeyType.String);
             keyTypes.Should().Contain(Models.KeyType.Long);
+        }
+
+        [Fact(DisplayName = "KafkaSendHandler can't handle wrong key type.")]
+        [Trait("Category", "Unit")]
+        public async Task CantHandleWrongKeyType()
+        {
+            // Arrange
+            var topicName = "test";
+            var key = "testKey";
+            var value = "testValue";
+            var isJson = false;
+            using var cts = new CancellationTokenSource();
+            var sender = new KafkaSendHandler(Mock.Of<IKafkaSender>(),
+                                              Mock.Of<IJsonValidator>(),
+                                              Mock.Of<ILogger<KafkaSendHandler>>());
+            // Act
+            var exception = await Record.ExceptionAsync(async () =>
+                                                        await sender.HandleAsync(
+                                                        topicName,
+                                                        (Models.KeyType)int.MinValue,
+                                                        key,
+                                                        value,
+                                                        isJson,
+                                                        cts.Token));
+
+            // Assert
+            exception.Should().NotBeNull().And.BeOfType<ArgumentOutOfRangeException>();
+        }
+
+        [Fact(DisplayName = "KafkaSendHandler validates json payload.")]
+        [Trait("Category", "Unit")]
+        public async Task ValidatesJsonPayload()
+        {
+            // Arrange
+            var topicName = "test";
+            var key = "testKey";
+            var value = "testValue";
+            var type = Models.KeyType.String;
+            var isJson = true;
+
+            var jsonValidatorMock = new Mock<IJsonValidator>(MockBehavior.Strict);
+            jsonValidatorMock.Setup(x => x.IsValid(value)).Returns(false);
+
+            using var cts = new CancellationTokenSource();
+
+            var sender = new KafkaSendHandler(Mock.Of<IKafkaSender>(),
+                                              jsonValidatorMock.Object,
+                                              Mock.Of<ILogger<KafkaSendHandler>>());
+            // Act
+            var exception = await Record.ExceptionAsync(async () =>
+                                                        await sender.HandleAsync(
+                                                        topicName,
+                                                        type,
+                                                        key,
+                                                        value,
+                                                        isJson,
+                                                        cts.Token));
+
+            // Assert
+            exception.Should().NotBeNull().And.BeOfType<ArgumentException>();
+        }
+
+        [Fact(DisplayName = "KafkaSendHandler validates json key.")]
+        [Trait("Category", "Unit")]
+        public async Task ValidatesJsonKey()
+        {
+            // Arrange
+            var topicName = "test";
+            var key = "testKey";
+            var value = "testValue";
+            var type = Models.KeyType.JSON;
+            var isJson = false;
+
+            var jsonValidatorMock = new Mock<IJsonValidator>(MockBehavior.Strict);
+            jsonValidatorMock.Setup(x => x.IsValid(key)).Returns(false);
+
+            using var cts = new CancellationTokenSource();
+
+            var sender = new KafkaSendHandler(Mock.Of<IKafkaSender>(),
+                                              jsonValidatorMock.Object,
+                                              Mock.Of<ILogger<KafkaSendHandler>>());
+            // Act
+            var exception = await Record.ExceptionAsync(async () =>
+                                                        await sender.HandleAsync(
+                                                        topicName,
+                                                        type,
+                                                        key,
+                                                        value,
+                                                        isJson,
+                                                        cts.Token));
+
+            // Assert
+            exception.Should().NotBeNull().And.BeOfType<ArgumentException>();
         }
     }
 }
