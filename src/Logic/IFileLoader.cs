ffa9300f9d1d2e8d22bad4b89b21b3a7d55a03dc
Nikita Tulenev <n1tulenev@gmail.com>
2022-05-14 18:43:49 +0300

    Add IFileLoader

---
+++ src/Logic/IFileLoader.cs
@@ -0,0 +1,7 @@
+﻿namespace Logic
+{
+    public interface IFileLoader
+    {
+        public Task<IEnumerable<KeyValuePair<string, string>>> LoadAsync(string filePath, CancellationToken ct);
+    }
+}
