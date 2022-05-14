4cf3d9f4378699b30148467b08741a9733a26f4e
Nikita Tulenev <n1tulenev@gmail.com>
2022-05-14 18:43:55 +0300

    Add File loader

---
+++ src/Logic/FileLoader.cs
@@ -0,0 +1,10 @@
+﻿namespace Logic
+{
+    public class FileLoader : IFileLoader
+    {
+        public Task<IEnumerable<KeyValuePair<string, string>>> LoadAsync(string filePath, CancellationToken ct)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
