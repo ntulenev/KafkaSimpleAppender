de3750394d61e65303d7ab7832ee50899384a014
Nikita Tulenev <n1tulenev@gmail.com>
2022-05-14 18:55:04 +0300

    Add FileLoader simple logic

--- src/Logic/FileLoader.cs
+++ src/Logic/FileLoader.cs
@@ -1,10 +1,26 @@
-﻿namespace Logic
+﻿using Newtonsoft.Json.Linq;
+
+namespace Logic
 {
     public class FileLoader : IFileLoader
     {
-        public Task<IEnumerable<KeyValuePair<string, string>>> LoadAsync(string filePath, CancellationToken ct)
+        public async Task<IEnumerable<KeyValuePair<string, string>>> LoadAsync(string filePath, CancellationToken ct)
         {
-            throw new NotImplementedException();
+            ArgumentNullException.ThrowIfNull(filePath);
+
+            var fileContent = await File.ReadAllTextAsync(filePath, ct);
+            var jArray = JArray.Parse(fileContent);
+
+            var result = new List<KeyValuePair<string, string>>();
+            foreach (var item in jArray)
+            {
+                var key = item["Key"].ToString();
+                var value = item["Value"].ToString();
+
+                result.Add(new KeyValuePair<string, string>(key, value));
+            }
+
+            return result;
         }
     }
 }
