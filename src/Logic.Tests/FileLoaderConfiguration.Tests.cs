6e544f58059eec7725b5fda4e085d1bef45afae4
Nikita Tulenev <n1tulenev@gmail.com>
2022-05-16 19:57:53 +0300

    Add FileLoaderConfigurationTests

---
+++ src/Logic.Tests/FileLoaderConfiguration.Tests.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+
+using Logic.Configuration;
+
+using Xunit;
+
+namespace Logic.Tests
+{
+    public class FileLoaderConfigurationTests
+    {
+        [Fact(DisplayName = " FileLoaderConfiguration could be created.")]
+        [Trait("Category", "Unit")]
+        public void CouldBeCreated()
+        {
+            // Act
+            var exception = Record.Exception(() => new FileLoaderConfiguration());
+
+            // Assert
+            exception.Should().BeNull();
+        }
+    }
+}
