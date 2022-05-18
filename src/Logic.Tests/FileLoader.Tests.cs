0c4abe976a8d4b7031a3fd4c1dafd32943358442
Nikita Tulenev <n1tulenev@gmail.com>
2022-05-18 21:30:25 +0300

    Add FileLoaderTests

---
+++ src/Logic.Tests/FileLoader.Tests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+using FluentAssertions;
+
+using Logic.Configuration;
+
+using Microsoft.Extensions.Options;
+
+using Moq;
+
+using Xunit;
+
+namespace Logic.Tests
+{
+    public class FileLoaderTests
+    {
+        [Fact(DisplayName = "FileLoader can't be created with null config.")]
+        [Trait("Category", "Unit")]
+        public void CantBeCreatedWithNullConfig()
+        {
+            // Act
+            var exception = Record.Exception(() => new FileLoader(null!));
+
+            // Assert
+            exception.Should().NotBeNull().And.BeOfType<ArgumentNullException>();
+        }
+
+        [Fact(DisplayName = "FileLoader could be created.")]
+        [Trait("Category", "Unit")]
+        public void CouldBeCreated()
+        {
+            // Arrange
+            var conf = new FileLoaderConfiguration()
+            {
+                FileKeyField = "Key",
+                FileValueField = "Value"
+            };
+            var configMock = new Mock<IOptions<FileLoaderConfiguration>>(MockBehavior.Strict);
+            configMock.Setup(x => x.Value).Returns(conf);
+
+            // Act
+            var exception = Record.Exception(() => new FileLoader(configMock.Object));
+
+            // Assert
+            exception.Should().BeNull();
+        }
+    }
+}
