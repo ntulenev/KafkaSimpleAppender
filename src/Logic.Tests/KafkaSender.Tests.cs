bf5cba1a54fd8294a2b96f37f590f85c947c310a
Nikita Tulenev <n1tulenev@gmail.com>
2022-05-21 19:54:25 +0300

    Add valid case collection sender tests

--- src/Logic.Tests/KafkaSender.Tests.cs
+++ src/Logic.Tests/KafkaSender.Tests.cs
@@ -221,5 +221,73 @@ namespace Logic.Tests
             // Assert
             exception.Should().NotBeNull().And.BeOfType<ArgumentException>();
         }
+
+        [Fact(DisplayName = "KafkaSender can send No key message collection.")]
+        [Trait("Category", "Unit")]
+        public async void CanSendNoKeyMessageCollection()
+        {
+            // Arrange
+            using var cts = new CancellationTokenSource();
+            var topic = new Topic("test");
+            var msg1 = new NoKeyMessage("test1");
+            var msg2 = new NoKeyMessage("test2");
+
+            var producerMock = new Mock<IProducer<Null, string>>(MockBehavior.Strict);
+            producerMock.Setup(x => x.ProduceAsync(topic.Name, It.Is<Message<Null, string>>(x => x.Value == msg1.Payload), cts.Token))
+                        .Returns(() => Task.FromResult(new DeliveryResult<Null, string>()));
+            producerMock.Setup(x => x.ProduceAsync(topic.Name, It.Is<Message<Null, string>>(x => x.Value == msg2.Payload), cts.Token))
+            .Returns(() => Task.FromResult(new DeliveryResult<Null, string>()));
+            producerMock.Setup(x => x.Dispose());
+
+            var builderMock = new Mock<IProducerBuilder>(MockBehavior.Strict);
+            builderMock.Setup(x => x.Build<Null>()).Returns(producerMock.Object);
+
+            var sender = new KafkaSender(builderMock.Object, Mock.Of<ILogger<KafkaSender>>());
+
+            var counter = 0;
+
+            // Act
+            var exception = await Record.ExceptionAsync(() => sender.SendAsync(topic, new[] { msg1, msg2 }, i => counter = i, cts.Token));
+
+            // Assert
+            exception.Should().BeNull();
+            counter.Should().Be(2);
+            producerMock.Verify(x => x.ProduceAsync(topic.Name, It.Is<Message<Null, string>>(x => x.Value == msg1.Payload), cts.Token), Times.Once);
+            producerMock.Verify(x => x.ProduceAsync(topic.Name, It.Is<Message<Null, string>>(x => x.Value == msg2.Payload), cts.Token), Times.Once);
+        }
+
+        [Fact(DisplayName = "KafkaSender can send key message collection.")]
+        [Trait("Category", "Unit")]
+        public async void CanSendKeyMessageCollection()
+        {
+            // Arrange
+            using var cts = new CancellationTokenSource();
+            var topic = new Topic("test");
+            var msg1 = new Message<int>(1, "test1");
+            var msg2 = new Message<int>(2, "test2");
+
+            var producerMock = new Mock<IProducer<int, string>>(MockBehavior.Strict);
+            producerMock.Setup(x => x.ProduceAsync(topic.Name, It.Is<Message<int, string>>(x => x.Value == msg1.Payload), cts.Token))
+                        .Returns(() => Task.FromResult(new DeliveryResult<int, string>()));
+            producerMock.Setup(x => x.ProduceAsync(topic.Name, It.Is<Message<int, string>>(x => x.Value == msg2.Payload), cts.Token))
+            .Returns(() => Task.FromResult(new DeliveryResult<int, string>()));
+            producerMock.Setup(x => x.Dispose());
+
+            var builderMock = new Mock<IProducerBuilder>(MockBehavior.Strict);
+            builderMock.Setup(x => x.Build<int>()).Returns(producerMock.Object);
+
+            var sender = new KafkaSender(builderMock.Object, Mock.Of<ILogger<KafkaSender>>());
+
+            var counter = 0;
+
+            // Act
+            var exception = await Record.ExceptionAsync(() => sender.SendAsync(topic, new[] { msg1, msg2 }, i => counter = i, cts.Token));
+
+            // Assert
+            exception.Should().BeNull();
+            counter.Should().Be(2);
+            producerMock.Verify(x => x.ProduceAsync(topic.Name, It.Is<Message<int, string>>(x => x.Value == msg1.Payload), cts.Token), Times.Once);
+            producerMock.Verify(x => x.ProduceAsync(topic.Name, It.Is<Message<int, string>>(x => x.Value == msg2.Payload), cts.Token), Times.Once);
+        }
     }
 }
