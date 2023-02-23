e12ef714dc66c7bc896b47e8d6a7a85085bff0e4
Nikita Tulenev <n1tulenev@gmail.com>
2023-02-23 18:34:59 +0100

    Remove non runnable Host and use ServiceCollection directly

--- src/KafkaSimpleAppender/Program.cs
+++ src/KafkaSimpleAppender/Program.cs
@@ -1,15 +1,15 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
 using Serilog;
 
-
 using Logic.Configuration;
 using Logic.Configuration.Validation;
 using Logic;
+
+using KafkaSimpleAppender;
 
 // It is unfortunate but we have to set it to Unknown first.
 Thread.CurrentThread.SetApartmentState(ApartmentState.Unknown);
@@ -19,46 +19,38 @@ Application.SetHighDpiMode(HighDpiMode.SystemAware);
 Application.EnableVisualStyles();
 Application.SetCompatibleTextRenderingDefault(false);
 
-var builder = new HostBuilder()
-  .ConfigureAppConfiguration((hostingContext, config) =>
-  {
-      config.AddJsonFile("appsettings.json", optional: true);
-  })
-  .ConfigureServices((hostContext, services) =>
-  {
-      services.AddScoped<KafkaSimpleAppender.UI>();
-      services.AddSingleton<IKafkaSender, KafkaSender>();
-      services.AddSingleton<IKafkaSendHandler, KafkaSendHandler>();
-      services.AddSingleton<IJsonValidator, JsonValidator>();
-      services.AddSingleton<IProducerBuilder, SimpleProducerBuilder>();
-      services.AddSingleton<IFileLoader, FileLoader>();
+var configuration = new ConfigurationBuilder()
+     .AddJsonFile($"appsettings.json");
+var config = configuration.Build();
 
-      services.Configure<BootstrapConfiguration>(hostContext.Configuration.GetSection(nameof(BootstrapConfiguration)));
-      services.Configure<FileLoaderConfiguration>(hostContext.Configuration.GetSection(nameof(FileLoaderConfiguration)));
-      services.AddSingleton<IValidateOptions<BootstrapConfiguration>, BootstrapConfigurationValidator>();
-      services.AddSingleton<IValidateOptions<FileLoaderConfiguration>, FileLoaderConfigurationValidator>();
-
-
-      var logger = new LoggerConfiguration()
-                       .ReadFrom.Configuration(hostContext.Configuration)
-                       .CreateLogger();
-
-      services.AddLogging(x =>
-      {
-          x.SetMinimumLevel(LogLevel.Information);
-          x.AddSerilog(logger: logger, dispose: true);
-      });
-  });
-
-var host = builder.Build();
-
-using (var serviceScope = host.Services.CreateScope())
+var serviceCollection = new ServiceCollection();
+serviceCollection.AddScoped<UI>();
+serviceCollection.AddSingleton<IKafkaSender, KafkaSender>();
+serviceCollection.AddSingleton<IKafkaSendHandler, KafkaSendHandler>();
+serviceCollection.AddSingleton<IJsonValidator, JsonValidator>();
+serviceCollection.AddSingleton<IProducerBuilder, SimpleProducerBuilder>();
+serviceCollection.AddSingleton<IFileLoader, FileLoader>();
+serviceCollection.Configure<BootstrapConfiguration>(config.GetSection(nameof(BootstrapConfiguration)));
+serviceCollection.Configure<FileLoaderConfiguration>(config.GetSection(nameof(FileLoaderConfiguration)));
+serviceCollection.AddSingleton<IValidateOptions<BootstrapConfiguration>, BootstrapConfigurationValidator>();
+serviceCollection.AddSingleton<IValidateOptions<FileLoaderConfiguration>, FileLoaderConfigurationValidator>();
+var logger = new LoggerConfiguration()
+                 .ReadFrom.Configuration(config)
+                 .CreateLogger();
+serviceCollection.AddLogging(x =>
 {
-    var services = serviceScope.ServiceProvider;
+    x.SetMinimumLevel(LogLevel.Information);
+    x.AddSerilog(logger: logger, dispose: true);
+});
+var provider = serviceCollection.BuildServiceProvider();
+
+using (var serviceScope = provider.CreateScope())
+{
+    var scopeProvider = serviceScope.ServiceProvider;
 
     try
     {
-        var form = services.GetRequiredService<KafkaSimpleAppender.UI>();
+        var form = scopeProvider.GetRequiredService<KafkaSimpleAppender.UI>();
         Application.Run(form);
     }
     catch (Exception ex)
